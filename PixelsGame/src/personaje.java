ee21bd7a66e066dc6a1b51096512414316cd1faf
Rack <ragontru25@gmail.com>
2019-11-07 11:09:15 +0100

    Update personaje
    Ya tengo cuando vaya arriba y abajo

--- PixelsGame/src/personaje.java
+++ PixelsGame/src/personaje.java
@@ -13,47 +13,65 @@ public class personaje extends Pizarra {
 	public void pintar(Graphics g, Tarjetero t) {
 		
 		int x0=250, y0=250, zoom=100;
-		
+/*		
 		//Abajo
 			//Pelo
 			g.setColor(Color.black);
 			g.fillRect(x0, y0, 50, 10);
 			g.fillRect(x0, y0, 10, 40);
 			g.fillRect(x0, y0+30, 20, 10);
-		//	g.drawLine(x0, y0+40, x0+2, y0+40);
-		//	g.drawLine(x0+30, y0+40, x0+50, y0+40);
-		//	g.drawLine(x0+50, y0, x0+50, y0+40);
+			g.fillRect(x0+40, y0, 10, 40);
+			g.fillRect(x0+30, y0+30, 20, 10);
 			
 			//Piel
 			g.setColor(Color.pink);
-		//	g.drawLine(x0+10, y0+10, x0+20, y0+20);
 			g.fillRect(x0+10, y0+10, 30, 20);
+			g.fillRect(x0+20, y0+30, 10, 10);
+			g.fillRect(x0-30, y0+40, 10, 10);
+			g.fillRect(x0+70, y0+40, 10, 10);
 			
 			//Chaleco
 			g.setColor(Color.green);
+			g.fillRect(x0-20, y0+40, 90, 10);
+			g.fillRect(x0+10, y0+50, 30, 30);
 			
 			//Pantalon
 			g.setColor(Color.blue);
+			g.fillRect(x0+10, y0+80, 30, 20);
+			g.fillRect(x0+10, y0+100, 10, 20);
+			g.fillRect(x0+30, y0+100, 10, 20);
 			
 			//Zapatos
 			g.setColor(Color.black);
+			g.fillRect(x0+10, y0+120, 10, 10);
+			g.fillRect(x0+30, y0+120, 10, 10);
 			
 		//Arriba
 			//Pelo
 			g.setColor(Color.black);
-			
+			g.fillRect(x0, y0, 50, 40);
+	
 			//Piel
-			g.setColor(Color.orange);
+			g.setColor(Color.pink);
+			g.fillRect(x0-30, y0+40, 10, 10);
+			g.fillRect(x0+70, y0+40, 10, 10);
 			
 			//Chaleco
 			g.setColor(Color.green);
+			g.fillRect(x0-20, y0+40, 90, 10);
+			g.fillRect(x0+10, y0+50, 30, 30);
 			
 			//Pantalon
 			g.setColor(Color.blue);
+			g.fillRect(x0+10, y0+80, 30, 20);
+			g.fillRect(x0+10, y0+100, 10, 20);
+			g.fillRect(x0+30, y0+100, 10, 20);
 			
 			//Zapatos
 			g.setColor(Color.black);
-			
+			g.fillRect(x0+10, y0+120, 10, 10);
+			g.fillRect(x0+30, y0+120, 10, 10);
+*/			
 		
 		//Izquierda
 			//Pelo
